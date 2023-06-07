2c12c33668d766212d1fd7d45a44c045e2e852cd
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2023-06-07 16:07:08 +0200

    hot fix

--- src/main/java/com/sdp/hms/controller/PublicController.java
+++ src/main/java/com/sdp/hms/controller/PublicController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.sdp.hms.exception.NotFoundException;
  *
  */
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/hms")
 public class PublicController {
